fdfe8f7c717fb9f68def5bbb27203146aa80ecef
Basit Ali <basitali03239129220@gmail.com>
2025-12-16 10:04:22 +0500

    Adding a comment to app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
-
+// Starting app here
 const app = express();
 
 app.set('view engine', 'pug');
